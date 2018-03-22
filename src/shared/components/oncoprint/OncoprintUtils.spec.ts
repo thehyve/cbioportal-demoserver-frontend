1172cc6cf41e1cbb567c9cc231da1f715992802a
Fedde Schaeffer <fedde@thehyve.nl>
2018-03-22 17:55:39 +0100

    Explore how to test makeGeneticTracksMobxPromise

--- src/shared/components/oncoprint/OncoprintUtils.spec.ts
+++ src/shared/components/oncoprint/OncoprintUtils.spec.ts
@@ -1,8 +1,62 @@
+import {
+    makeGeneticTracksMobxPromise,
+    percentAltered
+} from "./OncoprintUtils";
+import ResultsViewOncoprint from "./ResultsViewOncoprint";
+import * as _ from 'lodash';
+import {MobxPromise} from 'mobxpromise';
 import {assert} from 'chai';
-import {percentAltered} from "./OncoprintUtils";
+import sinon from 'sinon';
 
-describe('OncoprintUtils', ()=>{
-    describe('percentAltered',()=>{
+// This file uses type assertions to force functions that use overly specific
+// types as parameters to accept mocked literals believed to be sufficient
+// tslint:disable no-object-literal-type-assertion
+
+const ONCOPRINT_STORE_WITH_3_PATIENTS = {
+    samples: new MobxPromise(Promise.resolve()),
+    patients: new MobxPromise(Promise.resolve([
+        { "patientId": "TCGA-02-0001", "studyId": "gbm_tcga", "uniquePatientKey": "VENHQS0wMi0wMDAxOmdibV90Y2dh" },
+        { "patientId": "TCGA-02-0003", "studyId": "gbm_tcga", "uniquePatientKey": "VENHQS0wMi0wMDAzOmdibV90Y2dh" },
+        { "patientId": "TCGA-02-0006", "studyId": "gbm_tcga", "uniquePatientKey": "VENHQS0wMi0wMDA2OmdibV90Y2dh" }
+    ])),
+    genePanelInformation: new MobxPromise(Promise.resolve({
+        patients: {
+        }
+    })),
+    alteredSampleKeys: new MobxPromise(Promise.resolve()),
+    sequencedSampleKeysByGene: new MobxPromise(Promise.resolve()),
+    alteredPatientKeys: new MobxPromise(Promise.resolve()),
+    sequencedPatientKeysByGene: new MobxPromise(Promise.resolve())
+};
+
+describe('OncoprintUtils', () => {
+    describe('makeGeneticTracksMobxPromise', () => {
+        it('resolves with a single track if queried for a single gene', (done) => {
+            // given a three-patient oncoprint component queried for a single gene
+            const oncoprintComponent = {props: {store: {
+                ...ONCOPRINT_STORE_WITH_3_PATIENTS,
+                putativeDriverFilteredCaseAggregatedDataByUnflattenedOQLLine: new MobxPromise(Promise.resolve([
+                    {
+                        cases: {},
+                        oql: {}
+                    }
+                ]))
+            }}} as any as ResultsViewOncoprint;
+            // when the track promise is computed with this query
+            const trackPromise = makeGeneticTracksMobxPromise(oncoprintComponent, false);
+            // then it resolves with a single track
+            const resultingPromise = new MobxPromise({
+                await: () => [trackPromise],
+                invoke: () => {
+                    assert.lengthOf(trackPromise.result!, 1);
+                    done();
+                    return Promise.resolve();
+                }
+            });
+        });
+    });
+
+    describe('percentAltered', () => {
         it("returns the percentage with no decimal digits, for percentages >= 3", ()=>{
             assert.equal(percentAltered(3,100), "3%");
             assert.equal(percentAltered(20,100), "20%");
