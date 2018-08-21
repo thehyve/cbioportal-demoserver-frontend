8c0691ed68066adb5c631718118cea4b7ef06f81
oplantalech <oleguer@thehyve.nl>
2018-08-21 10:41:13 +0200

    Fix sorting of drug response and fix lint errors

--- src/pages/patientView/clinicalInformation/ClinicalInformationEventsTable.tsx
+++ src/pages/patientView/clinicalInformation/ClinicalInformationEventsTable.tsx
@@ -16,74 +16,92 @@ export interface IClinicalEventTableProps {
 class PatientEventTable extends LazyMobXTable<IPatientEventRow> {}
 
 interface IPatientEventRow {
-    columns: any[];
-};
+    columns: any;
+}
 
 export default class ClinicalInformationEventsTable extends React.Component<IClinicalEventTableProps, {}> {
 
-    private getDisplayValue(data:IPatientEventRow, key: string):number|string {
-        for (let i = 0; i < data.columns.length; i++) {
-            if (data.columns[i].key == key) {
-                if (isNaN(Number(data.columns[i].value))) {
-                    return data.columns[i].value;
-                }
-                return Number(data.columns[i].value);
+  public prepareData(eventData: ClinicalEvent[]) {
+
+      const tableData: IPatientEventRow[] = [];
+
+      let i = 0;
+      const j = 0;
+      for (i = 0; i < eventData.length; i++) {
+          const row = eventData[i];
+          if (row.eventType === "LAB_TEST") {
+
+              tableData.push({"columns" : row.attributes});
+          }
+      }
+
+      return tableData;
+  }
+
+  public render() {
+
+      const tableData = this.prepareData(this.props.data);
+      return (
+          <PatientEventTable
+                data={tableData}
+                columns={this.getColumnsAndData(tableData)}
+                showPagination={false}
+                showColumnVisibility={false}
+                className={styles.patientTable}
+                initialItemsPerPage={SHOW_ALL_PAGE_SIZE}
+                initialSortColumn={tableData[0].columns[1].key}
+                initialSortDirection={"desc"}
+                showFilter={(this.props.showFilter === false) ? false : true }
+                showCopyDownload={(this.props.showCopyDownload === false) ? false : true }
+          />
+      );
+  }
+
+  private getDisplayValue(data:IPatientEventRow, key: string):string {
+        for (const column of data.columns) {
+            if (column.key === key) {
+                return column.value;
             }
         }
         return "";
-        
-    }
-    
-    private getColumnsAndData(dataItems: IPatientEventRow[]): any {
-        if (!dataItems || !dataItems[0])
-            return [];
-        return dataItems[0].columns.map(({key, value}: {key: string, value: string}) =>
-            ({name: key,
-                      render: (data:IPatientEventRow) => <span>{this.getDisplayValue(data, key)}</span>,
-                      download: (data:IPatientEventRow) => this.getDisplayValue(data, key),
-                      sortBy: (data:IPatientEventRow)=> this.getDisplayValue(data, key)
-                }));
-    }
-    
 
-    public render() {
-
-        
-        const tableData = this.prepareData(this.props.data);
-        return (
-            <PatientEventTable
-                  data={tableData}
-                  columns={this.getColumnsAndData(tableData)}
-                  showPagination={false}
-                  showColumnVisibility={false}
-                  className={styles.patientTable}
-                  initialItemsPerPage={SHOW_ALL_PAGE_SIZE}
-                  initialSortColumn={tableData[0].columns[1].key}
-                  initialSortDirection={"desc"}
-                  showFilter={(this.props.showFilter === false) ? false : true }
-                  showCopyDownload={(this.props.showCopyDownload === false) ? false : true }
-            />
-        );
     }
 
+  private getSortingValue(data:IPatientEventRow, key: string):number|string {
+      for (const column of data.columns) {
+          if (column.key === key) {
+              if (isNaN(Number(column.value))) {
+                  return Number.NEGATIVE_INFINITY;
+              }
+              return Number(column.value);
+          }
+      }
+      return Number.NEGATIVE_INFINITY;
 
+  }
 
-    public prepareData(eventData: ClinicalEvent[]) {
+  private getColumnsAndData(dataItems: IPatientEventRow[]): any {
+      if (!dataItems || !dataItems[0])
+          return [];
 
-        const tableData: IPatientEventRow[] = [];
+      const columnsAndData: any = [];
 
-        let i = 0;
-        let j = 0;
-        for (i = 0; i < eventData.length; i++) {
-            let row = eventData[i];
-            if (row.eventType == "LAB_TEST") {
+      for (const column in dataItems[0].columns) {
+          if (dataItems[0].columns[column].key === "SUBTYPE") {
+              columnsAndData.push({name: dataItems[0].columns[column].key,
+                          render: (data:IPatientEventRow) => <span>{this.getDisplayValue(data, dataItems[0].columns[column].key)}</span>,
+                          download: (data:IPatientEventRow) => this.getDisplayValue(data, dataItems[0].columns[column].key),
+                          sortBy: (data:IPatientEventRow)=> this.getDisplayValue(data, dataItems[0].columns[column].key)
+                    });
+          } else {
+              columnsAndData.push({name: dataItems[0].columns[column].key,
+                      render: (data:IPatientEventRow) => <span>{this.getDisplayValue(data, dataItems[0].columns[column].key)}</span>,
+                      download: (data:IPatientEventRow) => this.getDisplayValue(data, dataItems[0].columns[column].key),
+                      sortBy: (data:IPatientEventRow)=> this.getSortingValue(data, dataItems[0].columns[column].key)
+                });
+          }
+      }
 
-                tableData.push({"columns" : row.attributes});
-            }
-        }
-
-        return tableData;
-    }
+      return columnsAndData;
+  }
 }
-
-
