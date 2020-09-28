5049e3faa7535642f8144ea5c7e5c377f8d62094
Ruslan Forostianov <ruslan@thehyve.nl>
2020-09-28 17:47:28 +0200

    Fix existing tests

--- src/shared/lib/oql/oqlfilter.spec.ts
+++ src/shared/lib/oql/oqlfilter.spec.ts
@@ -105,6 +105,7 @@ const THREE_GENE_TWO_SAMPLE_CNA_DATA = ([
             cytoband: '17q21.31',
             length: 8922,
         },
+        putativeDriver: true,
     },
     {
         __id: 8,
@@ -124,6 +125,7 @@ const THREE_GENE_TWO_SAMPLE_CNA_DATA = ([
             cytoband: '10q23.31',
             length: 11581,
         },
+        putativeDriver: true,
     },
     {
         __id: 9,
@@ -143,6 +145,7 @@ const THREE_GENE_TWO_SAMPLE_CNA_DATA = ([
             cytoband: '17p13.1',
             length: 4576,
         },
+        putativeDriver: true,
     },
 ] as any) as AnnotatedDiscreteCNAMolecularData[];
 // I believe these metadata to be all `new AccessorsForOqlFilter()` needs
